fe7818ec0e9da4c079a32f6b311b6c05f9460550
manavbatth <manavbatth@gmail.com>
2021-08-01 22:27:48 +0530

    regex file

--- src/MainEngine/Path.java
+++
@@ -1,7 +0,0 @@
-package MainEngine;
-
-public class Path {
-	public static String pathToWebpageDirectory = "WebPages/";
-	public static String pathToTextDirectory = "WebPages/Text/";
-	public static String txtDirectoryName = "Text/";
-}
